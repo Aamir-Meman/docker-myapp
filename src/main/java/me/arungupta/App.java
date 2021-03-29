4a14e64b343e1a1ac35c608a986034e201b02908
Aamir Meman <aamirmemon2292@gmail.com>
2021-03-29 19:11:54 -0400

    Update App.java

--- src/main/java/me/arungupta/App.java
+++ src/main/java/me/arungupta/App.java
@@ -5,6 +5,6 @@ package me.arungupta;
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World How are you Aamir!" );
+        System.out.println( "Hello World How are you Aamir?" );
     }
 }
